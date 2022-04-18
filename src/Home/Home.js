aaabb73fd8d265445d18309d4c5302505d601de8
wahi90 <mdshahed1098@gmail.com>
2022-04-18 15:08:05 +0600

    home section done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../Images/hospital.jpg'
 import './Home.css'
 
 const Home = () => {
     return (
+        <>
         <div className='home-container'>
             <img className='' src={logo} alt="" />
-            <div>
+            <div className='home-title'>
                 <h1 className='main-title'>Where your smile means <br />more.</h1>
                <div>
-               <h3 className='mt-4'>
+               <h3 className=''>
                 Treat your smile, treat your soul.
                 </h3>
-                <button>Book An Service</button>
+                <button className='btn btn-outline-primary mt-4 home-button '><Link to="/services">Book An Service</Link> </button>
                </div>
             </div>
         </div>
+        <div>
+            <h2 className='text-center mt-5'>Services</h2>
+        </div>
+        </>
+        
     );
 };
 
