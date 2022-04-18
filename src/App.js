fa29ac7524b689d6a222c2661983611016249f6a
wahi90 <mdshahed1098@gmail.com>
2022-04-18 22:01:12 +0600

    login form ui done and responsive also done

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Footer from './Footer/Footer';
 import NotFound from './NotFound/NotFound'
 import RequireAuth from './RequireAuth/RequireAuth';
 import Checkout from './Checkout/Checkout';
+import Register from './Register/Register';
 
 
 
@@ -30,6 +31,7 @@ function App() {
         <Route path='/blogs'element={<Blogs></Blogs>}></Route>
         <Route path='/about'element={<About></About>}></Route>
         <Route path='/login'element={<Login></Login>}></Route>
+        <Route path='/register'element={<Register></Register>}></Route>
         <Route path='*'element={<NotFound></NotFound>}></Route>
 
       </Routes>
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Welcome to Login</h1>
+        <> 
+        
+        <div className='login-container'>
+            <h3 className='text-center'>Welcome to Login</h3>
+          <div className='mx-auto'>
+          <Form  className=' form-container ' >
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+   
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  
+  
+  <Button variant="btn btn-outline-primary d-block w-100 mb-3" type="submit">
+    Submit
+  </Button>
+  <Form.Text className=" register-text">
+     Not a member <Link to="/register">Register</Link>
+    </Form.Text>
+</Form>
+          </div>
         </div>
+        
+        </>
+        
     );
 };
 
